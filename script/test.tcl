f46562fbe506f7b8fc7882a8d51172cce4a9aa36
xiaohaibiao <xiaohaibiao331@outlook.com>
2020-07-06 11:26:42 +0800

    add split excle.tcl

--- script/test.tcl
+++ script/test.tcl
@@ -4,54 +4,6 @@ package require biff
 
 namespace path biff
 
-#--------------------HammerDB auto test----------------------------
-# control the run time of vu
-proc runtimer {seconds} {
-  set x 0
-  set timerstop 0
-  while {!$timerstop} {
-    incr x
-    after 1000
-    if {![expr {$x % 60}]} {
-      set y [expr $x / 60]
-      puts "Timer: $y minutes elapsed"
-    }
-    update
-    if {[ vucomplete ] || $x eq $seconds} {set timerstop 1}
-  }
-  return
-}
-
-# start autopilot 
-proc autopilot { vu_list } {
-  global db_type
-  puts "-------------SETTING CONFIGURATION----------------"
-  dbset db $db_type
-  dbset bm TPC-C
-  set rest ""
-  diset tpcc [append rest $db_type "_driver"] timed
-  if {[string compare $db_type "ora"] != 0} {
-    set rest ""
-    diset tpcc [append rest $db_type "_timeprofile"] true
-  } else {
-    diset tpcc timeprofile true
-  }
-  loadscript
-
-  puts "------------------SEQUENCE STARTED-----------------"
-  foreach z $vu_list {
-    puts "$z VU TEST"
-    vuset vu $z
-    vuset logtotemp 1
-    vucreate
-    vurun
-    runtimer 600
-    vudestroy
-    after 5000
-  }
-  puts "-------------------TEST SEQUENCE COMPLETE-----------"
-}
-
 #---------------------Export the data to Excel----------------------
 
 proc export_data {log_fd row} {
@@ -62,17 +14,22 @@ proc export_data {log_fd row} {
   global index_map
   global excel_arr
 
-  while { [gets $log_fd data] >= 0} {
+  while { 1 } {
     set col 0
-    if {[regexp {([0-9]*)\s(Active\sVirtual\sUsers\sconfigured)} $data sub0 sub1] == 1} {
-      set col $index_map($sub1)
-    } else {
-      continue
+    while { [gets $log_fd data] >= 0} {
+      if {[regexp {([0-9]*)\s(Active\sVirtual\sUsers\sconfigured)} $data sub0 sub1] == 1} {
+        puts $data
+        set col $index_map($sub1)
+        break
+      } else {
+        continue
+      }
     }
 
     while { [gets $log_fd data] >= 0} {
       if {[regexp {TEST\sRESULT\D*([0-9]*)\D*([0-9]*)\sNOPM} $data sub0 sub1 sub2] == 1} {
         # set tpm $sub1
+        #puts $data
         append excel_arr [store_value [expr $row + $tpm_base] $col $sub1]
         # set nopm $sub2
         append excel_arr [store_value [expr $row + $nopm_base] $col $sub2]
@@ -85,11 +42,16 @@ proc export_data {log_fd row} {
     while { [gets $log_fd data] >= 0} {
       if {[regexp {([a-z]*)\s\S\s(\d*)\S(\d{2,}.\d{2,}%)\S\s(\d*)\S\s(\d*)\S\s(\d*)\S} $data sub0 procname exclusivetot per callnum avgpercall cumultot] == 1} {
         # set response time $sub1
+        #puts $data
         append excel_arr [store_value [expr $row + $response_time_base] $col $avgpercall]
         break
       } else {
         continue
       }
+    }
+
+    if {[eof $log_fd]} {
+      break
     }
   }
 }
@@ -144,52 +106,11 @@ proc init_excel { iterat_times file_name vu_list} {
   close $excel_fd
 }
 
-#-----------------------------entry---------------------------------
-
-proc entry { iterat_times file_name vu_list} {
-
-  set hammerdb_file ""
-  set row 0
-
-  # get the path of hammerdb.log file
-  switch -regexp $::tcl_platform(os) {
-    (Windows.NT) {
-      set hammerdb_file C:/hammerdb.log
-    }
-    (Linux) {
-      set hammerdb_file /tmp/hammerdb.log
-    }
-    default {
-      puts "can not find hammerdb.log in platform $tcl_platform(os)"
-      exit 0
-    }
-  }
-
-  # backup hammerdb.log file 
-  for {set i 1} {$i <= $iterat_times} {incr i} {
-
-    # start test and log file to hammerdb.log
-    autopilot $vu_list
-
-    if {[file exists $hammerdb_file]} {
-      file rename $hammerdb_file $i
-    } else {
-      puts "could no find $hammerdb_file"
-      continue
-    }
-  }
-
-  init_excel $iterat_times $file_name $vu_list
-}
-
-#-------------------------------------------------------------------
-puts "*************************start**************************"
-
 #set db_type mssqls
 set db_type mysql
 #set db_type ora
 set vu_list {1 2 4 8 16 32 64 128 256 512}
-set iterat_times 1
+set iterat_times 3
 set clock [clock format [clock seconds] -format %m-%d-%H%M%S]
 set excel_file_name "test_$clock"
 set index_map(0) "None"
@@ -200,6 +121,4 @@ set tpm_base 0
 set nopm_base [expr $tpm_base + $iterat_times + 1 + $offset]
 set response_time_base [expr $nopm_base + $iterat_times + 1 + $offset]
 
-entry $iterat_times $excel_file_name $vu_list
-
-puts "**************************done**************************"
+init_excel $iterat_times $excel_file_name $vu_list
